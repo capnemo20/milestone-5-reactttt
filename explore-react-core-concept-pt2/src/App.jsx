3c00d0fc1ee9653615e5aec472d24a7431bb027f
capnemo20 <z.sujon20@outlook.com>
2024-11-07 02:29:35 +0000

    39-2 Different way to handle events in React

--- explore-react-core-concept-pt2/src/App.jsx
+++ explore-react-core-concept-pt2/src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleClick() {
+    alert("Button Clicked");
+  }
+  const handleClick2 = () => {
+    alert("Button 2 Clicked");
+  };
 
+  const addToFive = (num)=>{
+    alert(num+5);
+  }
   return (
     <>
       <h3>React Core Concepts 2</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click2</button>
+      <button onClick={() => {alert("Third button clicked")}}>Click 3</button>
+      <button onClick={()=>addToFive(10)}>add 5</button>
     </>
-    
-  )
+  );
 }
 
-export default App
+export default App;
