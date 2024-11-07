0569290c23670bbcbd29fa3d8303c61d60d5073d
capnemo20 <z.sujon20@outlook.com>
2024-11-07 03:12:52 +0000

    39-3 A simple Introduction to State change in React

--- explore-react-core-concept-pt2/src/App.jsx
+++ explore-react-core-concept-pt2/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Counter from "./Counter";
 
 function App() {
   function handleClick() {
@@ -17,6 +18,7 @@ function App() {
   return (
     <>
       <h3>React Core Concepts 2</h3>
+      <Counter></Counter>
       <button onClick={handleClick}>Click Me</button>
       <button onClick={handleClick2}>Click2</button>
       <button onClick={() => {alert("Third button clicked")}}>Click 3</button>
