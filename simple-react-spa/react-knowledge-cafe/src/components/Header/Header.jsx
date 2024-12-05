70ebc1b9c6d6e2f588b88b5e63362ab9962dedb7
capnemo20 <z.sujon20@outlook.com>
2024-12-05 22:41:17 +0000

    42-2 Create Fake Data for the blogs and load data

---
+++ simple-react-spa/react-knowledge-cafe/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+    </div>
+  );
+};
+
+export default Header;
