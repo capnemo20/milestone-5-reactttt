e306dd9136c13ca2a17ef054034ad989574a1bb2
capnemo20 <z.sujon20@outlook.com>
2024-12-05 23:20:38 +0000

    42-3 Set Layout and make react responsive

--- simple-react-spa/react-knowledge-cafe/src/App.jsx
+++ simple-react-spa/react-knowledge-cafe/src/App.jsx
@@ -1,14 +1,16 @@
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
 import "./App.css";
 import Blogs from "./components/Blogs/Blogs";
+import Bookmarks from "./components/Bookmarks/Bookmarks";
 
 function App() {
-
-
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <div className="md:flex">
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   );
 }
